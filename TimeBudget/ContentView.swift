21ca21a8cd62e5f547962a13ff31d016ab1dd91e
Derrick Showers <derrick@derrickshowers.com>
2020-08-08 20:37:44 -0400

    Initial Commit

---
+++ TimeBudget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimeBudget
+//
+//  Created by Derrick Showers on 8/8/20.
+//  Copyright © 2020 Derrick Showers. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimeBudgetTests/TimeBudgetTests.swift
@@ -0,0 +1,34 @@
+//
+//  TimeBudgetTests.swift
+//  TimeBudgetTests
+//
+//  Created by Derrick Showers on 8/8/20.
+//  Copyright © 2020 Derrick Showers. All rights reserved.
+//
+
+import XCTest
+@testable import TimeBudget
+
+class TimeBudgetTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
