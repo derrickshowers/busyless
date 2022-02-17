1c187090efe05d2e1989b1edfdc696269d710bfd
Derrick Showers <derrick@derrickshowers.com>
2022-02-16 20:31:20 -0500

    More refactoring

--- Busyless/Containers/AddNewActivityView.swift
+++ Busyless/Containers/AddNewActivityView.swift
@@ -7,9 +7,7 @@
 //
 
 import SwiftUI
-import Intents
 import BusylessDataLayer
-import os
 
 struct AddNewActivityView: View {
 
@@ -23,45 +21,43 @@ struct AddNewActivityView: View {
 
     // MARK: - State
 
-    @State private var name: String
-    @State private var category: BLCategory?
-    @State private var hoursDuration: Int
-    @State private var minutesDuration: Int
-    @State private var createdAt: Date
-    @State private var notes: String
-
     @FocusState private var activityNameFocused: Bool?
 
-    private var isEditingExistingActivity: Bool = false
+    // MARK: - Views
 
-    private var readyToSave: Bool {
-        return !name.isEmpty && (hoursDuration != 0 || minutesDuration != 0)
+    private var cancelButton: some View {
+        Button("Cancel") {
+            dismiss()
+        }
+    }
+
+    private var doneButton: some View {
+        Button("Done") {
+            viewModel.save()
+            dismiss()
+        }.disabled(!viewModel.readyToSave)
+    }
+
+    private func durationStepper(with value: Binding<Int>, suffix: String) -> some View {
+        Stepper("\(value.wrappedValue) \(suffix)", value: value, in: 0...23).fixedSize()
     }
 
     // MARK: - Initialization
 
     init(viewModel: AddNewActivityViewModel) {
         self.viewModel = viewModel
-
-        let calculatedDuration = viewModel.activity.duration.asHoursAndMinutes
-        _name = State(initialValue: viewModel.activity.name ?? "")
-        _category = State(initialValue: viewModel.activity.category)
-        _createdAt = State(initialValue: viewModel.activity.createdAt ?? Date())
-        _notes = State(initialValue: viewModel.activity.notes ?? "")
-        _hoursDuration = State(initialValue: calculatedDuration.hours)
-        _minutesDuration = State(initialValue: calculatedDuration.minutes)
     }
 
     var body: some View {
             Form {
                 Section(header: Spacer()) {
-                    TextField("Activity Name", text: $name)
+                    TextField("Activity Name", text: $viewModel.name)
                         .focused($activityNameFocused, equals: true)
                         .autocapitalization(.words)
-                    NavigationLink(destination: CategorySelection(selectedCategory: $category)) {
+                    NavigationLink(destination: CategorySelection(selectedCategory: $viewModel.category)) {
                         Text("Category").bold()
                         Spacer()
-                        Text("\(category?.name ?? "")")
+                        Text("\($viewModel.category.wrappedValue?.name ?? "")")
                             .foregroundColor(.gray)
                             .lineLimit(1)
                     }
@@ -69,74 +65,33 @@ struct AddNewActivityView: View {
                         Text("Duration").bold()
                         Spacer()
                         VStack(alignment: .trailing) {
-                            Stepper("\(hoursDuration) hrs", value: $hoursDuration, in: 0...23).fixedSize()
+                            durationStepper(with: $viewModel.hoursDuration, suffix: "hrs")
                             Spacer()
-                            Stepper("\(minutesDuration) mins", value: $minutesDuration, in: 0...45, step: 15).fixedSize()
+                            durationStepper(with: $viewModel.minutesDuration, suffix: "mins")
                         }
                     }
                     HStack {
                         Text("When?").bold()
                         Spacer()
-                        DatePicker("When?", selection: $createdAt)
+                        DatePicker("When?", selection: $viewModel.createdAt)
                             .datePickerStyle(.compact)
                             .frame(maxWidth: 250, maxHeight: 25)
                     }
 
                 }
                 Section(header: Text("NOTES")) {
-                    TextEditor(text: $notes)
+                    TextEditor(text: $viewModel.notes)
                 }
             }
             .navigationBarTitleDisplayMode(.automatic)
-            .navigationBarTitle(isEditingExistingActivity ? "Edit Activity" : "Log New Activity")
             .navigationViewStyle(StackNavigationViewStyle())
             .onAppear {
                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.75) {
-                    activityNameFocused = name.isEmpty
+                    activityNameFocused = $viewModel.name.wrappedValue.isEmpty
                 }
             }
-            .navigationBarItems(leading:
-                Button(action: {
-                    dismiss()
-                }, label: {
-                    Text("Cancel")
-                }), trailing:
-                Button(action: {
-                    //self.addActivity()
-                    self.donateAddNewActivityIntent()
-                    //onComplete()
-                }, label: {
-                    Text("Done")
-                }).disabled(!readyToSave))
-
+            .navigationBarItems(leading: cancelButton, trailing: doneButton)
     }
-
-    // MARK: - Private Methods
-
-    private func donateAddNewActivityIntent() {
-        let intent = AddNewActivityIntent()
-        intent.name = self.name
-        let totalDuration = TimeInterval.calculateTotalDurationFrom(hours: hoursDuration, minutes: minutesDuration)
-        intent.durationInMinutes = NSNumber(value: (totalDuration / TimeInterval.oneHour) * TimeInterval.oneMinute)
-        let interaction = INInteraction(intent: intent, response: nil)
-        interaction.donate(completion: nil)
-    }
-}
-
-// MARK: - Core Data
-
-extension AddNewActivityView {
-
-    // TODO: Move to view model
-//    private func addActivity() {
-//        let activity = self.activity ?? Activity(context: managedObjectContext)
-//        activity.name = name
-//        activity.category = category
-//        activity.duration = TimeInterval.calculateTotalDurationFrom(hours: hoursDuration, minutes: minutesDuration)
-//        activity.notes = notes
-//        activity.createdAt = createdAt
-//        Activity.save(with: managedObjectContext)
-//    }
 }
 
 extension AddNewActivityView {
--- Busyless/ViewModels/AddNewActivityViewModel.swift
+++ Busyless/ViewModels/AddNewActivityViewModel.swift
@@ -9,25 +9,63 @@
 import Foundation
 import BusylessDataLayer
 import CoreData
+import Intents
 
 class AddNewActivityViewModel: ObservableObject {
 
     // MARK: - Properties
 
-    @Published var activity: Activity
+    @Published var name = ""
+    @Published var category: BLCategory?
+    @Published var hoursDuration = 0
+    @Published var minutesDuration = 15
+    @Published var createdAt = Date()
+    @Published var notes = ""
+
+    var readyToSave: Bool {
+        !name.isEmpty && (hoursDuration != 0 || minutesDuration != 0)
+    }
 
     // MARK: - Initialization
 
     private let dataStore: DataStore
-    private var childContext: NSManagedObjectContext {
-        didSet { childContext.parent = dataStore.context }
-    }
+    private var activity: Activity?
 
     init(dataStore: DataStore, activity: Activity? = nil) {
         self.dataStore = dataStore
-        self.childContext = NSManagedObjectContext(.mainQueue)
-        self.activity = activity ?? Activity(context: childContext)
+        self.activity = activity
 
-        childContext.parent = dataStore.context
+        if let activity = activity {
+            name = activity.name ?? ""
+            category = activity.category
+            hoursDuration = activity.duration.asHoursAndMinutes.hours
+            minutesDuration = activity.duration.asHoursAndMinutes.minutes
+            createdAt = activity.createdAt ?? Date()
+            notes = activity.notes ?? ""
+        }
+    }
+
+    // MARK: - Public Methods
+
+    func save() {
+        activity = activity ?? Activity(context: dataStore.context)
+        activity?.name = name
+        activity?.category = category
+        activity?.duration = TimeInterval.calculateTotalDurationFrom(hours: hoursDuration, minutes: minutesDuration)
+        activity?.createdAt = createdAt
+        activity?.notes = notes
+        Activity.save(with: dataStore.context)
+        donateAddNewActivityIntent()
+    }
+
+    // MARK: - Private Methods
+
+    private func donateAddNewActivityIntent() {
+        let intent = AddNewActivityIntent()
+        intent.name = name
+        let totalDuration = TimeInterval.calculateTotalDurationFrom(hours: hoursDuration, minutes: minutesDuration)
+        intent.durationInMinutes = NSNumber(value: (totalDuration / TimeInterval.oneHour) * TimeInterval.oneMinute)
+        let interaction = INInteraction(intent: intent, response: nil)
+        interaction.donate(completion: nil)
     }
 }
